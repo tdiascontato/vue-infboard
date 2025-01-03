5d026ef037327ac4367b14284cd81bf8af98d738
Tiago Dias <tdiascontato@gmail.com>
2025-01-03 12:36:45 -0300

    :wrench: settings on env and tsconfig

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="vite/client" />
+interface ImportMetaEnv {
+
+    readonly BASE_URL: string;
+  
+    // add more environment variables here if needed
+  
+  }
+  
+  
+  
+  interface ImportMeta {
+  
+    readonly env: ImportMetaEnv;
+  
+  }
+  
